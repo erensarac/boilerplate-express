9f5d569c5d5ebf810f481abd31840a1962142f86
Eren S <esarac@outlook.com.tr>
2024-04-09 23:23:36 +0000

    update root GET handler for serve html file

--- myApp.js
+++ myApp.js
@@ -1,11 +1,16 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
+// console.log("Hello World");
+
+// app.get("/", (req, res) => {
+//     res.send("Hello Express");
+// });
 
 app.get("/", (req, res) => {
-    res.send("Hello Express");
-});
+    const absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
+})
 
 
 
