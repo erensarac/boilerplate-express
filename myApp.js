c8e140049365a81db9394c9f6decddfbb6b66b80
Eren S <esarac@outlook.com.tr>
2024-04-09 23:43:42 +0000

    add root level request logger middleware

--- myApp.js
+++ myApp.js
@@ -9,6 +9,11 @@ let app = express();
 //     res.send("Hello Express");
 // });
 
+app.use((req, res, next) => {
+    console.log(req.method, req.path, "-", req.ip);
+    next();
+})
+
 app.use("/public", express.static(__dirname + '/public'))
 
 app.get("/", (req, res) => {
