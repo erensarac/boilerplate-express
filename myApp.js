08c288f5ad3a34f492f8678622c6de212a2ee231
Eren S <esarac@outlook.com.tr>
2024-04-09 23:49:17 +0000

    add echo server route

--- myApp.js
+++ myApp.js
@@ -32,6 +32,9 @@ app.get("/now", (req, res, next) => {
     next();
 }, (req, res) => res.json({time: req.time}))
 
+app.get("/:word/echo", (req, res) => {
+    res.json({echo: req.params.word})
+})
 
 
 
